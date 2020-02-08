b752232e034b30f63a36e62c88b6f08d73653ab7
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-08 13:57:36 -0700

    Add function to insert questions

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var timer = document.getElementById('countdown');
+var questionSpot = document.querySelector('.question');
+
+var questions = {
+    question: 'Commonly used data types do NOT include:',
+    options: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
+    correct: 3
+};
 
 function onQuestionsLoad() {
     if (document.getElementById('questionsPage')) {
         function scoreTimer() {
-            var timeLeft = 10;
+            var timeLeft = 9;
 
             var timeInterval = setInterval(function() {
                 timer.textContent = timeLeft;
                 timeLeft--;
 
-                if (timeLeft === 0) {
+                if (timeLeft === -2) {
                     timer.textContent = '';
                     clearInterval(timeInterval);
                     window.location.replace('results.html');
@@ -18,6 +25,19 @@ function onQuestionsLoad() {
         }
 
         scoreTimer();
+
+        function insertQuestion() {
+            var h4 = document.createElement('h4');
+            h4.textContent = questions.question;
+            questionSpot.appendChild(h4);
+
+            for (var i = 0; i < questions.options.length; i++) {
+                var button = document.createElement('button');
+                button.textContent = questions.options[i];
+                questionSpot.appendChild(button);
+            }
+        }
+        insertQuestion();
     }
 }
 onQuestionsLoad();
